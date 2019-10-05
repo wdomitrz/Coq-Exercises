8939f2362d6f9f08242ac20e90447c1d991727b4
wdomitrz <witekdomitrz@gmail.com>
2019-10-05 21:19:41 +0200

    Last commit

---
+++ MultiSortStackMachine.v
@@ -0,0 +1,190 @@
+Require Import Bool List Arith Bool_nat. 
+Set Implicit Arguments.
+
+
+(** * Typed Expressions *)
+
+(** In this section, we will build on the initial example by adding additional expression forms that depend on static typing of terms for safety. *)
+
+(** ** Source Language *)
+
+Inductive type : Set := Tint | Tbool.
+
+Inductive tbinop : type -> type -> type -> Set :=
+| TPlus : tbinop Tint Tint Tint
+| TTimes : tbinop Tint Tint Tint
+| TEq : forall t, tbinop t t Tbool
+| TLt : tbinop Tint Tint Tbool.
+
+
+Inductive texp : type -> Set :=
+| TNConst : nat -> texp Tint
+| TBConst : bool -> texp Tbool
+| TBinop : forall t1 t2 t, tbinop t1 t2 t -> texp t1 -> texp t2 -> texp t.
+
+Definition typeDenote (t : type) : Set :=
+  match t with
+    | Tint => nat
+    | Tbool => bool
+  end.
+
+Definition tbinopDenote arg1 arg2 res (b : tbinop arg1 arg2 res)
+  : typeDenote arg1 -> typeDenote arg2 -> typeDenote res :=
+  match b in tbinop arg1 arg2 res with
+    | TPlus => plus
+    | TTimes => mult
+    | TEq Tint => beq_nat
+    | TEq Tbool => eqb
+    | TLt => leb
+  end.
+
+
+Fixpoint texpDenote t (e : texp t) : typeDenote t :=
+  match e with
+    | TNConst n => n
+    | TBConst b => b
+    | TBinop b e1 e2 => (tbinopDenote b) (texpDenote e1) (texpDenote e2)
+  end.
+
+Eval simpl in texpDenote (TNConst 42).
+(** [= 42 : typeDenote Tint] *)
+
+(* begin hide *)
+Eval simpl in texpDenote (TBConst false).
+(* end hide *)
+Eval simpl in texpDenote (TBConst true).
+(** [= true : typeDenote Tbool] *)
+
+Eval simpl in texpDenote (TBinop TTimes (TBinop TPlus (TNConst 2) (TNConst 2))
+  (TNConst 7)).
+(** [= 28 : typeDenote Tint] *)
+
+Eval simpl in texpDenote (TBinop (TEq Tint) (TBinop TPlus (TNConst 2) (TNConst 2))
+  (TNConst 7)).
+(** [= false : typeDenote Tbool] *)
+
+Eval simpl in texpDenote (TBinop TLt (TBinop TPlus (TNConst 2) (TNConst 2))
+  (TNConst 7)).
+(** [= true : typeDenote Tbool] *)
+
+
+(** ** Target Language *)
+
+Definition tstack := list type.
+
+Inductive tinstr : tstack -> tstack -> Set :=
+| TiNConst : forall s, nat -> tinstr s (Tint :: s)
+| TiBConst : forall s, bool -> tinstr s (Tbool :: s)
+| TiBinop : forall arg1 arg2 res s,
+  tbinop arg1 arg2 res
+  -> tinstr (arg1 :: arg2 :: s) (res :: s).
+
+Inductive tprog : tstack -> tstack -> Set :=
+| TNil : forall s, tprog s s
+| TCons : forall s1 s2 s3,
+  tinstr s1 s2
+  -> tprog s2 s3
+  -> tprog s1 s3.
+
+Fixpoint vstack (ts : tstack) : Set :=
+  match ts with
+    | nil => unit
+    | t :: ts' => typeDenote t * vstack ts'
+  end%type.
+
+Definition tinstrDenote ts ts' (i : tinstr ts ts') : vstack ts -> vstack ts' :=
+  match i with
+    | TiNConst _ n => fun s => (n, s)
+    | TiBConst _ b => fun s => (b, s)
+    | TiBinop _ b => fun s =>
+      let '(arg1, (arg2, s')) := s in
+        ((tbinopDenote b) arg1 arg2, s')
+  end.
+
+
+Fixpoint tprogDenote ts ts' (p : tprog ts ts') : vstack ts -> vstack ts' :=
+  match p with
+    | TNil _ => fun s => s
+    | TCons i p' => fun s => tprogDenote p' (tinstrDenote i s)
+  end.
+
+(** ** Translation *)
+
+Fixpoint tconcat ts ts' ts'' (p : tprog ts ts') : tprog ts' ts'' -> tprog ts ts'' :=
+  match p with
+    | TNil _ => fun p' => p'
+    | TCons i p1 => fun p' => TCons i (tconcat p1 p')
+  end.
+
+Fixpoint tcompile t (e : texp t) (ts : tstack) : tprog ts (t :: ts) :=
+  match e with
+    | TNConst n => TCons (TiNConst _ n) (TNil _)
+    | TBConst b => TCons (TiBConst _ b) (TNil _)
+    | TBinop b e1 e2 => tconcat (tcompile e2 _)
+      (tconcat (tcompile e1 _) (TCons (TiBinop _ b) (TNil _)))
+  end.
+
+Eval simpl in tprogDenote (tcompile (TNConst 42) nil) tt.
+(** [= (42, tt) : vstack (Tint :: nil)] *)
+
+Eval simpl in tprogDenote (tcompile (TBConst true) nil) tt.
+(** [= (true, tt) : vstack (Tbool :: nil)] *)
+
+Eval simpl in tprogDenote (tcompile (TBinop TTimes (TBinop TPlus (TNConst 2)
+  (TNConst 2)) (TNConst 7)) nil) tt.
+(** [= (28, tt) : vstack (Tint :: nil)] *)
+
+Eval simpl in tprogDenote (tcompile (TBinop (TEq Tint) (TBinop TPlus (TNConst 2)
+  (TNConst 2)) (TNConst 7)) nil) tt.
+(** [= (false, tt) : vstack (Tbool :: nil)] *)
+
+Eval simpl in tprogDenote (tcompile (TBinop TLt (TBinop TPlus (TNConst 2) (TNConst 2))
+  (TNConst 7)) nil) tt.
+(** [= (true, tt) : vstack (Tbool :: nil)] *)
+
+(** %\smallskip{}%The compiler seems to be working, so let us turn to proving that it _always_ works. *)
+
+
+(** ** Translation Correctness *)
+
+
+(** Again, we need to strengthen the theorem statement so that the induction will go through.  This time, to provide an excuse to demonstrate different tactics, I will develop an alternative approach to this kind of proof, stating the key lemma as: *)
+(* We need an analogue to the [app_assoc_reverse] theorem that we used to rewrite the goal in the last section.  We can abort this proof and prove such a lemma about [tconcat].
+*)
+
+
+Lemma tconcat_correct : forall ts ts' ts'' (p : tprog ts ts') (p' : tprog ts' ts'')
+  (s : vstack ts),
+  tprogDenote (tconcat p p') s
+  = tprogDenote p' (tprogDenote p s).
+Proof.
+  induction p; intros.
+  * reflexivity.
+  * intros.
+    simpl.
+    apply IHp.
+Qed.
+
+
+Lemma tcompile_correct' : forall t (e : texp t) ts (s : vstack ts),
+  tprogDenote (tcompile e ts) s = (texpDenote e, s).
+Proof.
+  induction e; intros; try reflexivity.
+  * simpl.
+    rewrite tconcat_correct.
+    rewrite tconcat_correct.
+    rewrite IHe1.
+    rewrite IHe2.
+    reflexivity.
+Qed.
+
+Theorem tcompile_correct : forall t (e : texp t),
+  tprogDenote (tcompile e nil) tt = (texpDenote e, tt).
+Proof.
+  intros.
+  apply (tcompile_correct' e nil tt).
+Qed.
+
+Extraction tcompile.
+
+(* End: *)
---
+++ zal19.v
@@ -0,0 +1,204 @@
+Set Implicit Arguments.
+Require Import  List.
+Require Import Eqdep.
+Require Import Eqdep_dec.
+Require Import Omega.
+
+
+(* Coq assignment - ZPF 2019 - due to 07.05.2019
+
+Fill the definitions and prove the lemmas given below (replace 
+Admitted with Qed).
+
+It is not allowed to: 
+1. change/erase given definitions and lemma statements,
+   section header/footer, variable declaration, etc.,
+2. introduce your own axioms, parameters, variables, hypotheses etc.,   
+3. import other modules,
+4. define Ltac tactics.
+
+It is allowed to:
+1. introduce your own definitions and auxiliary lemmas,
+2. change the order of the lemmas to prove,
+3. add comments. 
+
+Submit your solution via email before 23:59 on 07.05.2019. 
+You should submit one file named zal.v containing your proofs.
+
+The author of the assignment and the grader is Daria Walukiewicz-Chrząszcz.
+
+*)
+
+Section filterL.
+(*------------------------------------------------------------------
+Nondependent case : filter on lists
+--------------------------------------------------------------------
+*)
+
+Print list.
+
+Variable A : Type.
+Variable P : A -> Prop.
+
+Variable P_dec : forall x, {P x}+{~P x}.
+
+Fixpoint filterL (l:list A) : list A :=
+match l with
+| nil => nil
+| cons a l' => if P_dec a then cons a (filterL l') else filterL l'
+end.
+
+Fixpoint countPL (l : list A) := 
+match l with 
+| nil => O
+| cons x l' => if P_dec x then S (countPL l') else countPL l'
+end.
+
+
+(* Prove lemmas cPL1 and cPL2 *)
+
+Print Forall.
+
+Lemma cPL1: forall (l:list A), countPL l = 0 -> 
+           Forall (fun x => ~P x) l.
+Proof.
+Admitted.
+
+
+Lemma cPL2: forall (l:list A), countPL l = length l -> Forall P l.
+Proof.
+Admitted.
+
+
+(* in case of troubles: think about the lengths of the lists *)
+
+End filterL.
+
+
+Section filterV.
+(*------------------------------------------------------------------
+Dependent case: filter on vectors
+--------------------------------------------------------------------
+*)
+
+
+Variable A : Type.
+Variable P : A -> Prop.
+
+Variable P_dec : forall x, {P x}+{~P x}.
+
+
+Inductive vector : nat -> Type :=
+  | Vnil : vector 0
+  | Vcons : forall {n}, A -> vector n -> vector (S n).
+
+  Arguments Vcons {_} _ _.
+
+(*
+Write the definition of countPV on vectors; it should correspond to countPL 
+Use keyword Fixpoint
+*)
+
+(*
+Write the definition of filterV on vectors; it should correspond to filterL.
+Use keyword Fixpoint
+*)
+
+(*
+ForallV is Forall on vectors
+*)
+
+Print Forall.
+
+Inductive ForallV (P:A-> Prop): forall {n:nat}, vector n -> Prop :=
+    Forall_Vnil : ForallV P Vnil
+  | Forall_Vcons : forall (x : A) (n:nat) (v : vector n),
+                  P x -> ForallV P v -> ForallV P (Vcons x v).
+
+
+
+(* 
+Write the definition of the last element of a nonempty vector. 
+Do it twice:
+- using tactics in proof-mode
+- using Fixpoint and match
+
+Fill:
+
+Definition lastOfNonemptyByProof {n:nat} (v:vector (S n)): A := 
+Definition lastOfNonemptyByHand {n:nat} (v:vector (S n)) : A := 
+*)
+
+Variable e1 e2 e3:A.
+
+(*
+and test it:
+
+Eval compute in (lastOfNonemptyByProof (Vcons e1 (Vcons e2 (Vcons e3 Vnil)))). 
+Eval compute in (lastOfNonemptyByHand (Vcons e1 (Vcons e2 (Vcons e3 Vnil)))). 
+*)
+
+
+(* 
+Prove lemmas cPV1 and cPV2
+*)
+
+Lemma cPV1: forall (n:nat)(v:vector n), countPV v = 0 -> 
+           ForallV (fun x => ~P x) v.
+Proof.
+Admitted.
+
+
+Lemma cPV2: forall (n:nat) (v:vector n), countPV v = n -> ForallV P v.
+Proof.
+Admitted.
+
+(* Recall that UIP_refl nat is provable in Coq *)
+
+Check (UIP_refl nat).
+
+
+(*
+Prove the following inversion lemma 
+*)
+
+
+Lemma cPVInversion: forall (n:nat) (a:A) (v:vector n), 
+      S n = countPV (Vcons a v) -> (P a /\ n = countPV v).
+Proof.
+Admitted.
+
+(*
+Prove cPVfilterVIdentity
+*)
+
+
+Lemma cPVfilterVIdentity: forall (n:nat) (v:vector n) (d: n = countPV v),
+filterV v = match d in _= m return vector m with
+                            | eq_refl => v
+                            end.
+Proof.
+Admitted.
+
+
+(* 
+cPVtc is a type-cast needed to formulate the lemma given below
+*)
+
+Lemma cPVtc : forall {n:nat} (v:vector n),  countPV v = countPV (filterV v).
+Proof.
+Admitted.
+
+(* 
+Use the lemmas proved above to show that filterV is idempotent
+*)
+ 
+
+Lemma filterV_idem: forall {n:nat} (v:vector n),
+      filterV (filterV v) = match cPVtc v in _= m return vector m with
+                            | eq_refl => filterV v
+                            end.
+Proof.
+Admitted.
+
+End filterV.
